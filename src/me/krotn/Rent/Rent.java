b15fc45ee7c944953d25beae2e420ffb53f6c4c7
Karl Otness <karl@otness.com>
2011-05-21 16:58:29 -0500

    Added enable and disable log messages to the plugin.
    The plugin now prints log messages when it is enabled and disabled.

--- src/me/krotn/Rent/Rent.java
+++ src/me/krotn/Rent/Rent.java
@@ -1,13 +1,17 @@
 package me.krotn.Rent;
 
+import java.util.logging.Logger;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Rent extends JavaPlugin{
+	
+	Logger log = Logger.getLogger("Minecraft");
+	
 	public void onEnable(){
-		
+		log.info("Rent enabled");
 	}
 	
 	public void onDisable(){
-		
+		log.info("Rent disabled");
 	}
 }
