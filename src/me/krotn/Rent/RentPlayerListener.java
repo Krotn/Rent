5dd2f05ebc232f28f656d5c98368dfd2f3384311
Karl Otness <karl@otness.com>
2011-05-22 12:22:09 -0500

    Added some documentation comments to RentPlayerListener.

--- src/me/krotn/Rent/RentPlayerListener.java
+++ src/me/krotn/Rent/RentPlayerListener.java
@@ -4,6 +4,15 @@ import java.util.logging.Logger;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerListener;
 
+/**
+ * This class listens for player events from Bukkit.
+ * 
+ * Currently it receives only PlayerJoin events.
+ * 
+ * @see org.bukkit.event.player
+ * @see org.bukkit.event.player.PlayerJoinEvent
+ */
+
 public class RentPlayerListener extends PlayerListener{
 	
 	private final Rent plugin;
