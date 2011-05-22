a517bdb2addcb2ca34cae19ae6e742900ea2152c
Karl Otness <karl@otness.com>
2011-05-22 17:53:48 -0500

    Added setup() method to RentDatabaseManager.
    This method takes care of creating the necessary tables in the database.
    The convenience method isConnected() was also added.

--- src/me/krotn/Rent/RentDatabaseManager.java
+++ src/me/krotn/Rent/RentDatabaseManager.java
@@ -48,4 +48,33 @@ public class RentDatabaseManager {
 			log.severe("RENT: Could not close database connection!");
 		}
 	}
+	
+	public boolean isConnected(){
+		if(conn==null){
+			return false;
+		}
+		try{
+			if(conn.isClosed()){
+				return false;
+			}
+		}catch(SQLException e){
+			log.warning("RENT: Unable to determine if database is connected!");
+		}
+		return true;
+	}
+	
+	public void setup(){
+		if(!isConnected()){
+			connect();
+		}
+		log.info("RENT: Setting up the database...");
+		try{
+			Statement statement = conn.createStatement();
+			statement.executeUpdate("CREATE TABLE Months (id INTEGER PRIMARY KEY,ref TEXT,cost REAL);");
+			statement.executeUpdate("CREATE TABLE Players (id INTEGER PRIMARY KEY,name TEXT);");
+			statement.executeUpdate("CREATE TABLE Logins (id INTEGER PRIMARY KEY,player_id INTEGER,month_id INTEGER,FOREIGN KEY(player_id) REFERENCES Players(id),FOREIGN KEY (month_id) REFERENCES Months(id));");
+		}catch(SQLException e){
+			log.severe("RENT: Unable to set-up the database!");
+		}
+	}
 }
