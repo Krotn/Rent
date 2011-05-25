7dcbf1f8823de4fe6cf671e9a8a16c0b91530358
Karl Otness <karl@otness.com>
2011-05-25 17:51:42 -0500

    Added RentDateUtils to the project.

---
+++ src/me/krotn/Rent/RentDateUtils.java
@@ -0,0 +1,10 @@
+package me.krotn.Rent;
+
+/**
+ * This class provides several convenience methods for working with dates.
+ * It will be used extensively by the rent plugin.
+ *
+ */
+public class RentDateUtils {
+	
+}
