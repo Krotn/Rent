0e5130fe8ddf936ce867cf1cd68005f77f06d29e
Karl Otness <karl@otness.com>
2011-05-26 16:09:45 -0500

    Added some documentation to RentLogManager.

--- src/me/krotn/Rent/RentLogManager.java
+++ src/me/krotn/Rent/RentLogManager.java
@@ -2,14 +2,28 @@ package me.krotn.Rent;
 
 import java.util.logging.Logger;
 
+/**
+ * This class applies standard formatting to all log messages it handles.
+ * Currently, it adds the text "RENT :" to the beginning of each log message.
+ */
 public class RentLogManager {
 	private Logger log;
 	private String prefix = "RENT: ";
 	
+	/**
+	 * Constructs a {@code RentLogManager} that uses the specified {@code Logger}
+	 * @param log The {@code Logger} to use.
+	 */
 	public RentLogManager(Logger log){
 		this.log=log;
 	}
 	
+	/**
+	 * Constructs a {@code RentLogManager} that uses the specified {@code Logger} and applies
+	 * the specified prefix.
+	 * @param log The logger to use.
+	 * @param prefix The prefix to add to the beginning of each log message.
+	 */
 	public RentLogManager(Logger log, String prefix){
 		this(log);
 		this.prefix = prefix;
