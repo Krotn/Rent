b4234e320922c0b6573a4fa3ee7528bfb03efc96
Karl Otness <karl@otness.com>
2011-05-28 18:03:15 -0500

    Added getCurrentMonth() to RentDateUtils.
    This method returns the current month identification String.

--- src/me/krotn/Rent/RentDateUtils.java
+++ src/me/krotn/Rent/RentDateUtils.java
@@ -1,5 +1,8 @@
 package me.krotn.Rent;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * This class provides several convenience methods for working with dates.
  * It will be used extensively by the rent plugin.
@@ -14,5 +17,16 @@ public class RentDateUtils {
 	 */
 	public RentDateUtils(RentDatabaseManager dbMan){
 		this.dbMan = dbMan;
+	}
+	
+	/**
+	 * Returns the current human-readable date {@code String} (eg. "jan11").
+	 * @return The current human readable date identification {@code String}.
+	 */
+	public String getCurrentMonth(){
+		SimpleDateFormat dateFormat = new SimpleDateFormat("MMMyy");
+		Date currentDate = new Date();
+		String toReturn = dateFormat.format(currentDate);
+		return toReturn.toLowerCase();
 	}
 }
