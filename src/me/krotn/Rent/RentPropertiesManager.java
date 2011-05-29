2df1150326e0d5e49be0200b6d7990f8c860bcea
Karl Otness <karl@otness.com>
2011-05-28 19:05:20 -0500

    Added documentation to RentPropertiesManager.

--- src/me/krotn/Rent/RentPropertiesManager.java
+++ src/me/krotn/Rent/RentPropertiesManager.java
@@ -25,10 +25,17 @@ public class RentPropertiesManager {
 	RentLogManager logManager;
 	Hashtable<String,String> defaults = new Hashtable<String,String>();
 	
+	/**
+	 * Constructs a RentPropertiesManager using the default properties file name ("config.properties").
+	 */
 	public RentPropertiesManager(){
 		this(defaultPropertiesName);
 	}
 	
+	/**
+	 * Constructs a RentPropertiesManager using the given properties file name.
+	 * @param propertiesName The name of the properties file to use.
+	 */
 	public RentPropertiesManager(String propertiesName){
 		this.propertiesName = propertiesName;
 		propFile = new File(RentDirectoryManager.getPathInDir(propertiesName));
@@ -39,10 +46,16 @@ public class RentPropertiesManager {
 		checkDefaults();
 	}
 	
+	/**
+	 * Puts the default values into the default values hashtable.
+	 */
 	private void setupDefaults(){
 		defaults.put("defaultCost", "10");
 	}
 	
+	/**
+	 * Makes sure that all the default config nodes are in the config file. If any are missing they are added.
+	 */
 	private void checkDefaults(){
 		Enumeration keys = defaults.keys();
 		while(keys.hasMoreElements()){
@@ -53,6 +66,9 @@ public class RentPropertiesManager {
 		}
 	}
 	
+	/**
+	 * Creates the properties file using the name specified at the construction of the RentPropertiesManager.
+	 */
 	public void setup(){
 		try {
 			propFile.createNewFile();
@@ -60,9 +76,11 @@ public class RentPropertiesManager {
 			logManager.severe("Error creating the properties file!");
 			e.printStackTrace();
 		}
-		setProperty("testProperty","woahTesting");
 	}
 	
+	/**
+	 * Updates the values in the properties cache from the actual properties file.
+	 */
 	public void update(){
 		try {
 			FileInputStream in = new FileInputStream(propFile);
@@ -74,14 +92,28 @@ public class RentPropertiesManager {
 		}
 	}
 	
+	/**
+	 * Returns the value of the specified properties node.
+	 * @param propertyName The {@code String} requested properties node.
+	 * @return The value of the properties node or {@code null} if the node is unset.
+	 */
 	public String getProperty(String propertyName){
 		return prop.getProperty(propertyName);
 	}
 	
+	/**
+	 * Returns whether or not the properties file specified at the construction of this {@code RentPropertiesManager} exists.
+	 * @return {@code true} the properties file exists {@code false} otherwise.
+	 */
 	public boolean fileExists(){
 		return propFile.exists();
 	}
 	
+	/**
+	 * Sets the value of the specified properties node to the specified value.
+	 * @param propertyName The name of the properties node to set.
+	 * @param propertyValue The value to which the properties node should be set.
+	 */
 	public void setProperty(String propertyName,String propertyValue){
 		prop.setProperty(propertyName,propertyValue);
 		try{
