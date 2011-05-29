f9be9c690d137354bae456cec3a7489037729e03
Karl Otness <karl@otness.com>
2011-05-29 16:40:21 -0500

    Added getIndividualRate() to RentCalculationsManager.
    This method calculates the server cost divided among the players who
    logged in duing the given month.

--- src/me/krotn/Rent/RentCalculationsManager.java
+++ src/me/krotn/Rent/RentCalculationsManager.java
@@ -10,4 +10,18 @@ public class RentCalculationsManager {
 		this.dbMan = this.plugin.getDatabaseManager();
 		this.logMan = this.plugin.getLogManager();
 	}
+	
+	/**
+	 * Returns the amount an individual player would owe had they logged in during the given month.
+	 * @param monthID The {@code int} database ID of the requested month.
+	 * @return The {@code double} amount a player would owe for logging in during the given month.
+	 */
+	public double getIndividualRate(int monthID){
+		if(!dbMan.monthExists(dbMan.getMonthFromID(monthID))){
+			return 0;
+		}
+		int numPlayersWhoLoggedIn = dbMan.getPlayersWhoLoggedIn(monthID).size();
+		double monthlyCost = dbMan.getMonthCost(monthID);
+		return monthlyCost/numPlayersWhoLoggedIn;
+	}
 }
