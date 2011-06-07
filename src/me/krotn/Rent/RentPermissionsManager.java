c0905a167e599baa3d12a53db55ac8753d311742
Karl Otness <karl@otness.com>
2011-06-06 22:05:32 -0500

    The checkPermission() method of RentPermissionsManager now accepts a CommandSender.
    This means that the Rent commands will be able to be used by the
    server console.

--- src/me/krotn/Rent/RentPermissionsManager.java
+++ src/me/krotn/Rent/RentPermissionsManager.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import com.nijiko.permissions.PermissionHandler;
 import com.nijikokun.bukkit.Permissions.Permissions;
 
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 
@@ -47,23 +48,29 @@ public class RentPermissionsManager {
 	}
 	
 	/**
-	 * This method returns whether or not a player has a given permission.
-	 * @param player The {@code Player} in question.
+	 * This method returns whether or not a player has a given permission.<br/>
+	 * If the given {@code CommandSender} is not a player they are assumed to be the console
+	 * and are given <i><b>all</b></i> permissions.
+	 * @param sender The {@code CommandSender} in question.
 	 * @param node The {@code String} permissions node to test <b>WITHOUT</b> "Rent." internally "Rent."
      * is added to the start of the node.
 	 * @return {@code true} the player has this permission. {@code false} otherwise.
 	 */
-	public boolean checkPermission(Player player,String node){
-		if(node==null){
-			return false;
+	public boolean checkPermission(CommandSender sender,String node){
+		if(sender instanceof Player){
+			Player player = (Player) sender;
+			if(node==null){
+				return false;
+			}
+			String workingNode = baseNode+node;
+			if(permHandler!=null){
+				return permHandler.has(player, workingNode);
+			}
+			if(player.isOp() && !(workingNode.equalsIgnoreCase("Rent.untracked"))){
+				return true;
+			}
+			return nonOpsPermissions.contains(workingNode);
 		}
-		String workingNode = baseNode+node;
-		if(permHandler!=null){
-			return permHandler.has(player, workingNode);
-		}
-		if(player.isOp() && !(workingNode.equalsIgnoreCase("Rent.untracked"))){
-			return true;
-		}
-		return nonOpsPermissions.contains(workingNode);
+		return true; //Probably console.
 	}
 }
