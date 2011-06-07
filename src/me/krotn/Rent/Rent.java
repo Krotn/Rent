10aae1283612d6bf991b38e9c8ac5244037016ac
Karl Otness <karl@otness.com>
2011-06-06 22:19:29 -0500

    Rent now supports commands from the console.
    The server console can use all commands except "/rent info" on itself.

--- src/me/krotn/Rent/Rent.java
+++ src/me/krotn/Rent/Rent.java
@@ -39,10 +39,9 @@ public class Rent extends JavaPlugin{
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String commandLabel, String[] args){
 		dateUtils.sanityCheck();
-		Player player = (Player) sender;
-		if(!(sender instanceof Player)){
-			logManager.warning("Cannot use commands through console!");
-			return false;
+		boolean isPlayer = false;
+		if((sender instanceof Player)){
+			isPlayer = true;
 		}
 		if(!command.getName().equalsIgnoreCase("rent")){
 			return true; //No further need of this.
@@ -52,79 +51,80 @@ public class Rent extends JavaPlugin{
 		}
 		if(args[0].equalsIgnoreCase("info")){
 			//Execute info command
-			if(args.length == 1 && permMan.checkPermission(player, "info.self")){
+			if(args.length == 1 && permMan.checkPermission(sender, "info.self")&&isPlayer){
 				//This is an info on oneself.
+				Player player = (Player) sender; //this will kill the function if not a player.
 				if(dbman.playerExists(player.getName())){
-					player.sendRawMessage(ChatColor.AQUA+propman.getProperty("selfInfoMessage")+" "+ChatColor.GREEN+propman.getProperty("currencyPrefix")+calcMan.getAmountPlayerOwes(dbman.getPlayerID(player.getName())));
+					player.sendMessage(ChatColor.AQUA+propman.getProperty("selfInfoMessage")+" "+ChatColor.GREEN+propman.getProperty("currencyPrefix")+calcMan.getAmountPlayerOwes(dbman.getPlayerID(player.getName())));
 					//Above line sends player the amount they owe.
 					return true;
 				}
 			}
-			else if(permMan.checkPermission(player, "info.other")&&args.length>1){
+			else if(permMan.checkPermission(sender, "info.other")&&args.length>1){
 				//This is not a self request
 				String requestedPlayer = args[1];
 				if(dbman.playerExists(requestedPlayer)){
 					int playerID = dbman.getPlayerID(requestedPlayer);
 					String currencyPrefix = propman.getProperty("currencyPrefix");
 					double playerOwes = calcMan.getAmountPlayerOwes(playerID);
-					player.sendRawMessage(ChatColor.AQUA+requestedPlayer+": "+ChatColor.GREEN+currencyPrefix+new Double(playerOwes).toString());
+					sender.sendMessage(ChatColor.AQUA+requestedPlayer+": "+ChatColor.GREEN+currencyPrefix+new Double(playerOwes).toString());
 					return true;
 				}
 				else{
-					player.sendRawMessage(ChatColor.RED+"No such player!");
+					sender.sendMessage(ChatColor.RED+"No such player!");
 					return true;
 				}
 			}
 		}//End of "info" command block.
-		if(args[0].equalsIgnoreCase("addpayment")&&permMan.checkPermission(player, "payment.add")&&args.length>=3){
+		if(args[0].equalsIgnoreCase("addpayment")&&permMan.checkPermission(sender, "payment.add")&&args.length>=3){
 			String requestedPlayer = args[1];
 			double amount = new Double(args[2]).doubleValue();
 			if(amount<0){
-				player.sendRawMessage(ChatColor.RED+"Amount must be positive!");
+				sender.sendMessage(ChatColor.RED+"Amount must be positive!");
 				return true;
 			}
 			if(!dbman.playerExists(requestedPlayer)){
-				player.sendRawMessage(ChatColor.RED+"No such player!");
+				sender.sendMessage(ChatColor.RED+"No such player!");
 				return true;
 			}
 			dbman.addPlayerPayments(dbman.getPlayerID(requestedPlayer), amount);
-			player.sendRawMessage(ChatColor.GREEN+"Added: "+amount+" to player: "+requestedPlayer+".");
+			sender.sendMessage(ChatColor.GREEN+"Added: "+amount+" to player: "+requestedPlayer+".");
 			return true;
 		}
-		if(args[0].equalsIgnoreCase("dedpayment")&&permMan.checkPermission(player, "payment.deduct")&&args.length>=3){
+		if(args[0].equalsIgnoreCase("dedpayment")&&permMan.checkPermission(sender, "payment.deduct")&&args.length>=3){
 			String requestedPlayer = args[1];
 			double amount = new Double(args[2]).doubleValue();
 			if(amount<0){
-				player.sendRawMessage(ChatColor.RED+"Amount must be positive!");
+				sender.sendMessage(ChatColor.RED+"Amount must be positive!");
 				return true;
 			}
 			if(!dbman.playerExists(requestedPlayer)){
-				player.sendRawMessage(ChatColor.RED+"No such player!");
+				sender.sendMessage(ChatColor.RED+"No such player!");
 				return true;
 			}
 			dbman.subtractPlayerPayments(dbman.getPlayerID(requestedPlayer),amount);
-			player.sendRawMessage(ChatColor.GREEN+"Subtracted: "+amount+" from player: "+requestedPlayer+".");
+			sender.sendMessage(ChatColor.GREEN+"Subtracted: "+amount+" from player: "+requestedPlayer+".");
 			return true;
 		}
-		if(args[0].equalsIgnoreCase("adjmonth")&&permMan.checkPermission(player,"month.adjustcost")&&args.length>=3){
+		if(args[0].equalsIgnoreCase("adjmonth")&&permMan.checkPermission(sender,"month.adjustcost")&&args.length>=3){
 			String requestedMonth = args[1];
 			double newCost = new Double(args[2]).doubleValue();
 			if(!dbman.monthExists(requestedMonth)){
-				player.sendRawMessage(ChatColor.RED+"No such month!");
+				sender.sendMessage(ChatColor.RED+"No such month!");
 				return true;
 			}
 			dbman.setMonthCost(dbman.getMonthID(requestedMonth), newCost);
-			player.sendRawMessage(ChatColor.GREEN+"Set cost for "+requestedMonth+" to: "+newCost+".");
+			sender.sendMessage(ChatColor.GREEN+"Set cost for "+requestedMonth+" to: "+newCost+".");
 			return true;
 		}
-		if(args[0].equalsIgnoreCase("dump")&&permMan.checkPermission(player,"dump")){
+		if(args[0].equalsIgnoreCase("dump")&&permMan.checkPermission(sender,"dump")){
 			try{
 				RentDumpManager dumpMan = new RentDumpManager(this);
 				dumpMan.dump();
-				player.sendRawMessage(ChatColor.GREEN+"Dump produced!");
+				sender.sendMessage(ChatColor.GREEN+"Dump produced!");
 				return true;
 			}catch(Exception e){
-				player.sendRawMessage(ChatColor.RED+"Error producing dump!");
+				sender.sendMessage(ChatColor.RED+"Error producing dump!");
 				e.printStackTrace();
 				return true;
 			}
