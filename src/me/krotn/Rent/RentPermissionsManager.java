f287df35311d88374a46da2068a6f9b2fbd14874
Karl Otness <karl@otness.com>
2011-08-26 13:17:39 -0500

    Rent now supports Bukkit's SuperPerms.

--- src/me/krotn/Rent/RentPermissionsManager.java
+++ src/me/krotn/Rent/RentPermissionsManager.java
@@ -1,51 +1,22 @@
 package me.krotn.Rent;
 
-import java.util.ArrayList;
-
-import com.nijiko.permissions.PermissionHandler;
-import com.nijikokun.bukkit.Permissions.Permissions;
-
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.ConsoleCommandSender;
-import org.bukkit.entity.Player;
-import org.bukkit.plugin.Plugin;
+import org.bukkit.permissions.Permissible;
 
 /**
  * This class handles various permissions related tasks for the Rent plugin.
  *
  */
 public class RentPermissionsManager {
-	private Rent plugin;
-	private PermissionHandler permHandler;
-	private RentLogManager logMan;
 	private String baseNode = "Rent.";
-	private ArrayList<String> nonOpsPermissions = new ArrayList<String>();
 	
 	/**
 	 * Constructs a RentPermissionsManager using the given Rent plugin.
 	 * @param plugin The plugin to use when getting necessary objects.
 	 */
 	public RentPermissionsManager(Rent plugin){
-		this.plugin = plugin;
-		this.logMan = this.plugin.getLogManager();
-		setupNonOpsPermissions();
-		Plugin permissionsPlugin = this.plugin.getServer().getPluginManager().getPlugin("Permissions");
-		if (this.permHandler == null) {
-	          if (permissionsPlugin != null) {
-	              this.permHandler = ((Permissions) permissionsPlugin).getHandler();
-	          } else {
-	              logMan.info("Permission system not detected, defaulting to OP");
-	              permHandler = null;
-	          }
-	      }
-	}
-	
-	/**
-	 * This method sets up the permissions list for non-opped players.
-	 * It adds various permissions nodes to {@code nonOpsPermissions}.
-	 */
-	private void setupNonOpsPermissions(){
-		nonOpsPermissions.add("Rent.info.self");
+	    
 	}
 	
 	/**
@@ -58,19 +29,13 @@ public class RentPermissionsManager {
 	 * @return {@code true} the player has this permission. {@code false} otherwise.
 	 */
 	public boolean checkPermission(CommandSender sender,String node){
-		if(sender instanceof Player){
-			Player player = (Player) sender;
+		if(sender instanceof Permissible){
+			Permissible permble = (Permissible) sender;
 			if(node==null){
 				return false;
 			}
 			String workingNode = baseNode+node;
-			if(permHandler!=null){
-				return permHandler.has(player, workingNode);
-			}
-			if(player.isOp() && !(workingNode.equalsIgnoreCase("Rent.untracked"))){
-				return true;
-			}
-			return nonOpsPermissions.contains(workingNode);
+			return permble.hasPermission(workingNode);
 		}
 		else if(sender instanceof ConsoleCommandSender){
 			return true; //Probably console.
